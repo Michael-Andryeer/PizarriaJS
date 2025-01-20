0af0d4ef32bab96dfff33364b9fdb8cbd79db401
michael-andryeer <andryeerm@gmail.com>
2025-01-20 03:01:46 -0300

    Tratativa de erros e setando o Cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,26 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import 'express-async-errors';
 import { router } from "./routes";
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+     res.status(400).json({
+      error: err.message
+      });
+  }
+
+   res.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  });
+});
 
 app.listen(3333, () => {
   console.log("Servidor rodando na porta 3333");
